898230857f67d4305206f6bceed2c36632242489
Rafiul <md.rafiul.aiub@gmail.com>
2024-12-29 16:24:18 +0600

    check the marge wihout conflict

--- src/main/java/class1.java
+++ src/main/java/class1.java
@@ -4,4 +4,7 @@ public class class1 {
     public static void main(String[] args) {
         System.out.println("Zim");
     }
+    public void Samman(){
+        System.out.println("hello test");
+    }
 }
--- src/main/java/class2.java
+++ src/main/java/class2.java
@@ -1,5 +1,6 @@
 public class class2 {
     public static void main(String[] args) {
         System.out.println("zim2");
+        System.out.println(33);
     }
 }
